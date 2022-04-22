ea25b6e3b0076f13130071518f89785319e53b94
dhruv50ae <linkdp2012@gmail.com>
2022-04-22 10:29:55 +0100

    Added box and texts for each character

--- main.py
+++ main.py
@@ -4,7 +4,17 @@ from pytesseract import pytesseract
 pytesseract.tesseract_cmd = "C:/Program Files/Tesseract-OCR/tesseract.exe"
 
 img = cv.imread("Photos/textnum.png")
+height, width, c = img.shape
 
-wordsInImage = pytesseract.image_to_string(img)
+letterBoxes = pytesseract.image_to_boxes(img)
 
-print(wordsInImage)
+for box in letterBoxes.splitlines():
+    box = box.split()
+    print(box)
+    x, y, w, h = int(box[1]), int(box[2]), int(box[3]), int(box[4])
+    cv.rectangle(img, (x, height-y), (w, height-h), (0, 0, 255), 3)
+    cv.putText(img, box[0], (x, height-h+32),
+    cv.FONT_HERSHEY_TRIPLEX, 1, (0, 0, 255), 1)
+
+cv.imshow("Window", img)
+cv.waitKey(0)
